8f7a1b995e141fe0b3ac0b3046a6b7b9b34f47f2
ambati-vedapriya <122984576+ambati-vedapriya@users.noreply.github.com>
2023-03-01 15:31:13 +0530

    restaurant-app
    this is the JavaScript assignment

---
+++ resturant-app/menu.js
@@ -0,0 +1,57 @@
+const menu = {
+    item1: {
+      name: "Sandwich",
+      cost: 100,
+      category: "entrees",
+    },
+    item2: {
+      name: "Burger",
+      cost: 150,
+      category: "entrees",
+    },
+    item3: {
+      name: "Momos",
+      cost: 150,
+      category: "entrees",
+    },
+    item4: {
+      name: "Shahi Paneer",
+      cost: 200,
+      category: "Main Course",
+    },
+    item5: {
+      name: "Butter Chicken",
+      cost: 300,
+      category: "Main Course",
+    },
+    item6: {
+      name: "Gulab Jamun",
+      cost: 50,
+      category: "Desserts",
+    },
+    item7: {
+      name: "Chocolate Brownie",
+      cost:100,
+      category:"Desserts",
+    },
+    item8: {
+      name: "Coke",
+      cost: 50,
+      category: "Beverages",
+    },
+    item9: {
+      name: "Chocolate Shake",
+      cost: 70,
+      category: "Beverages",
+    },
+    item10: {
+      name: "Chessy pull-Apart Breda",
+      cost: 270,
+      category: "Appetizer",
+    },
+    item11: {
+      name: "Hot corn and Chesse Dip",
+      cost: 170,
+      category: "Appetizer",
+    },
+};
