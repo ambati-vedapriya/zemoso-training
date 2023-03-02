39a67dabad5a7eea9742efdbffd7c6d191ed3166
ambati-vedapriya <122984576+ambati-vedapriya@users.noreply.github.com>
2023-03-02 16:11:26 +0530

    changes are done
    verify it

---
+++ resturant-app/tables.js
@@ -0,0 +1,34 @@
+var tables = {
+  table1: {
+    id: 1,
+    cost: 0,
+    items: 0,
+    orders: {},
+  },
+  table2: {
+    id: 2,
+    cost: 0,
+    items: 0,
+    orders: {},
+  },
+  table3: {
+    id: 3,
+    cost: 0,
+    items: 0,
+    orders: {},
+  },
+  table4: {
+    id: 4,
+    cost: 0,
+    items: 0,
+    orders: {},
+  },
+  table5: {
+    id: 5,
+    cost: 0,
+    items: 0,
+    orders: {},
+  },
+
+
+};
